e17c30652a1a373ad194d4b4b63a948225b8f8cd
lordcsd <dimgbachinonso@gmail.com>
2023-01-12 10:25:12 +0100

    added index in outside src

---
+++ lib/src/index.d.ts
@@ -0,0 +1,6 @@
+export declare const lgas: (state: string) => string[] | undefined;
+export declare const all: () => {
+    state: string;
+    lgas: string[];
+}[];
+export declare const states: () => string[];
